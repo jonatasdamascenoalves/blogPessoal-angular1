ed3f484040319ec56d8dfb7d72751deb27122f7a
Jo Je Ja <jonatasdamascenoalves@gmail.com>
2020-04-22 16:14:52 -0300

    Implementação do getAllpostagens no componente Feed.

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -21,7 +21,7 @@ import{HttpClientModule} from '@angular/common/http'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
     HttpClientModule
   ],
   providers: [],
