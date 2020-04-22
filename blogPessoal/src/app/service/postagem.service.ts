57919691c7fb399b00e133c0dc6cd7ebd574b766
Jo Je Ja <jonatasdamascenoalves@gmail.com>
2020-04-22 15:09:02 -0300

    Criação do endPoint getAllpostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+//crud - create, ready, update e delete
+
+getAllpostagens(){
+
+    return this.http.get('http://31.220.57.14/postagens')
+}
 
   
 }
