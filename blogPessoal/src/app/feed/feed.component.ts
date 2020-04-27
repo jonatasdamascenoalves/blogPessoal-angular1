1286d1ff7e1de9db288f95c3d68902a92c76959b
Jo Je Ja <jonatasdamascenoalves@gmail.com>
2020-04-27 14:40:26 -0300

    Implementação do postPostagem

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -12,6 +12,8 @@ export class FeedComponent implements OnInit {
 
   listaPostagens: Postagem []
 
+  postagem:Postagem = new Postagem
+
 
   constructor(private postagemService: PostagemService) { }
 
@@ -22,6 +24,13 @@ export class FeedComponent implements OnInit {
 findallPostagens(){
   this.postagemService.getAllpostagens().subscribe((resp: Postagem[])=>{
     this.listaPostagens = resp
+  })
+}
+
+publicar(){
+  this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+    this.postagem = resp
+    location.assign('/feed')
   })
 }
 
--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Postagem } from '../model/Postagem';
 
 
 @Injectable({
@@ -10,12 +11,14 @@ export class PostagemService {
 
   constructor(private http: HttpClient) { }
 
-//crud - create, ready, update e delete
+//crud - create(post), read(get), update(put) e delete
 
 getAllpostagens(){
-
-    return this.http.get('http://31.220.57.14/postagens')
+return this.http.get('http://31.220.57.14:8080/postagens')
 }
 
-  
+postPostagem(postagem: Postagem){
+  return this.http.post('http://31.220.57.14:8080/postagens',postagem)
+}
+
 }
